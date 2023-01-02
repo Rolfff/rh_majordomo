6ecab95828d9261673761f6133c8789a33b1dc98
Rolf <vcp-dauborn.de>
2023-01-02 19:56:44 +0100

    Chamge versions.

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.9.99',
+            'typo3' => '10.0.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
