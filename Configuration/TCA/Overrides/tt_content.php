b1b4ca212004bf63fddf012c2ae7d4784f687f26
Rolf <vcp-dauborn.de>
2023-04-22 22:27:24 +0200

    technical implementation

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,17 +1,4 @@
 <?php 
-/*
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['rhmajordomo_feplugin'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
-    'rhmajordomo_feplugin',
-    // Flexform configuration schema file
-    'FILE:EXT:rh_majordomo/Configuration/FlexForms/Feplugin.xml'
-);
-*/
 declare(strict_types=1);
 
 
