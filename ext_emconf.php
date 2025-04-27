919de4a0446efb705aaeab5c602260b1584cfb0c
Rolf <vcp-dauborn.de>
2025-04-27 14:41:17 +0200

    first step -> https://blog.matthaa.de/fehler-beim-upgrade-auf-typo3-12-something-went-wrong/

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-11.9.99',
+            'typo3' => '11.0.0-12.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
